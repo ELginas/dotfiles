bce1aa9ba9651bace0db6e27bdb99660ce6df2cc
ELginas <gintaras.z123@yahoo.com>
2023-12-17 06:33:26 +0200

    penrose

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,11 @@
 if status is-login; and test "$DISPLAY" = ""; and test "$XDG_VTNR" = "1"
-    startx
+    exec startx
 end
 
 abbr -a hx helix
 set -x EDITOR helix
 set -x GPG_TTY (tty)
 fish_add_path ~/.cargo/bin
+fish_add_path ~/.local/bin
+fish_add_path /opt/cuda/bin/
 set PATH (string match -v "/home/user/qtile/.venv/bin" $PATH)
