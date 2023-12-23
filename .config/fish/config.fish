ad2aaa45f56d4f0482f65a1476c9f4312107d44d
ELginas <gintaras.z123@yahoo.com>
2023-12-23 08:56:38 +0200

    cleaning up

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,12 @@ if status is-login; and test "$DISPLAY" = ""; and test "$XDG_VTNR" = "1"
 end
 
 abbr -a hx helix
+abbr -a pa pacman
 set -x EDITOR helix
 set -x GPG_TTY (tty)
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
 fish_add_path /opt/cuda/bin/
-set PATH (string match -v "/home/user/qtile/.venv/bin" $PATH)
+abbr -a -- cb 'cargo c && cargo b --release'
+set -x ANDROID_NDK ~/Android/Sdk/ndk/26.1.10909125/
+set -x ANDROID_HOME ~/Android/Sdk/
